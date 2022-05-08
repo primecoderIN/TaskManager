7f3d36730254e3d73b9d7bc13f37f8c7fdac477f
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-05-08 11:11:17 +0530

    Put method and not found endpoint implemented

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,8 @@ const {
     createNewTask,
     deleteTask,
     updateTask,
-    getTask
+    getTask,
+    editTask
 } = require("../controllers/tasks")
 
 // router.route("/").get(getAllTasks)
@@ -16,7 +17,11 @@ const {
 // router.route("/:id").get(getTask)
 
 router.route("/").get(getAllTasks).post(createNewTask)
-router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask)
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask).put(editTask)
 
 
 module.exports = router; //This will be imported in index.js 
+
+
+//Put is used to replace the existing entry
+//Patch is used for partial update. Whatever we send in the body only that portion will be updated in the entry
