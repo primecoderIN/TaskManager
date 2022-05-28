e3531113aac3fc2cee5c2949a4cd6227f035bd01
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-05-28 14:33:09 +0530

    Changes for deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 //Server setup
-const port = process.env.PORT;
+const port = process.env.PORT | 3000;
 const hostname = "localhost"
 //We are first connecting to DB then starting server using promisses and async await
 const URI = process.env.MONGO_URI;
